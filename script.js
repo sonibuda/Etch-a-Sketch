3b17f43fd3ed72180b58b93939377d657991308b
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2025-02-17 09:23:24 -0700

    Create 16x16 grid with Flexbox.

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
-    const newContent = document.createTextNode("test");
-    div.appendChild(newContent);
+    //const newContent = document.createTextNode("test");
+    //div.appendChild(newContent);
     div.classList.add('allDivs');
     mainContainer.appendChild(div);
 }
 
+//<div onmouseover="this.style.backgroundColor='red'">myDIV.</div>
 
